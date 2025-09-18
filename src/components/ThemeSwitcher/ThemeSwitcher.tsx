9ef293612c39800c6fc93277fd87d44a7705c76c
Pedro Garcia <garciasan@esat-alumni.com>
2025-09-18 23:56:09 +0200

    fix: ThemeSwitcher classes updated

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -10,7 +10,7 @@ export const ThemeSwitcher = () => {
   const { theme, setLightMode, setDarkMode } = useTheme();
 
   return (
-    <div className={cx('align-end', 'color-switch-block')}>
+    <div className={cx('align-end', 'themeswitcher__Button--margin')}>
       <Button onClick={() => setLightMode()} toggle={theme === 'lightmode'}>
         <Icon icon="Sun" size="small" />
       </Button>
@@ -21,6 +21,3 @@ export const ThemeSwitcher = () => {
     </div>
   );
 };
-
-// toggledependency inicia en false, puede ser boolean, se usa para sacar una condicion, todos los botones, si esto es true
-// adquieren clase que indica toggled, dependes de crear condiciones excluyentes para que 2 en una pareja nunca sean a la vez,
