68450707948319be7bcffcdc74cfd9e07b9d5275
Pedro Garcia <garciasan@esat-alumni.com>
2025-09-18 19:34:33 +0200

    fix: cleaned up app + main and defined basic page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import { Icon } from './assets/icons/Icon';
 import pokemon_logo_dark from './assets/Pokemon-logo-dark.svg';
-import './App.scss';
+import classNames from 'classnames/bind';
+import theme from './App.module.scss';
+
+const cx = classNames.bind(theme);
 
 function App() {
   return (
     <>
-      <img src={pokemon_logo_dark} alt="pokemon logo outlined" title="Pokemon logo" />
-      <p className="display-titles">sinnoh</p>
-      <Icon icon="Sun" color="light" size="large" />
+      <div className={cx('align-end')}>
+        <Icon icon="Sun" color="light" size="small" />
+        <Icon icon="Moon" color="light" size="small" />
+      </div>
+
+      <header>
+        <img src={pokemon_logo_dark} alt="pokemon logo outlined" title="Pokemon logo" className={cx('pokemon-logo')} />
+        <h1 className={cx('display-titles', 'text-align-center')}>sinnoh</h1>
+      </header>
+
+      <p className={cx('sinnoh-introductory-text', 'paragraph-s-regular')}>
+        The Sinnoh region, formerly Hisui, is a region of the Pokémon world located north of Kanto, Johto, and Hoenn. It
+        is the setting of Pokémon Diamond, Pearl, Platinum, Brilliant Diamond, Shining Pearl, and Legends: Arceus; the
+        latter explores the region's earlier history when it was still known as Hisui. It was the fourth core series
+        region to be introduced.
+      </p>
     </>
   );
 }
