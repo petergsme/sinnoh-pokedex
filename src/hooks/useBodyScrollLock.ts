29f8770e88f3da24b3c385f52c50f4fab700927f
Pedro Garcia <garciasan@esat-alumni.com>
2025-09-20 01:26:04 +0200

    fix: useBodyScrollLock hook simplified as to keep gradient on body background.

--- src/hooks/useBodyScrollLock.ts
+++ src/hooks/useBodyScrollLock.ts
@@ -7,19 +7,11 @@ interface UseBodyScrollLockOptions {
 export const useBodyScrollLock = ({ enabled = true }: UseBodyScrollLockOptions) => {
   useEffect(() => {
     if (!enabled) return;
-    const scrollY = window.scrollY;
 
     document.body.style.overflow = 'hidden';
-    document.body.style.position = 'fixed';
-    document.body.style.top = `-${scrollY}px`;
-    document.body.style.width = '100%';
 
     return () => {
-      document.body.style.overflow = 'unset';
-      document.body.style.position = '';
-      document.body.style.top = '';
-      document.body.style.width = '';
-      window.scrollTo(0, scrollY);
+      document.body.style.overflow = '';
     };
   }, [enabled]);
 };
