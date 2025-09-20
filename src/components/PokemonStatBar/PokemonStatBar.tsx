1e12d752132c9c842dc62a17abcf2a7e3772759d
Pedro Garcia <garciasan@esat-alumni.com>
2025-09-20 13:56:18 +0200

    feat: PokemonStatBar component and styles created

---
+++ src/components/PokemonStatBar/PokemonStatBar.tsx
@@ -0,0 +1,39 @@
+import { useTheme } from '../../contexts/ThemeContext/useTheme';
+import { Icon, type IconName } from '../../assets/icons/Icon';
+import classNames from 'classnames/bind';
+import theme from './PokemonStatBar.module.scss';
+
+const cx = classNames.bind(theme);
+
+interface PokemonStatBarProps {
+  type?: 'attack' | 'defense';
+  value: number;
+}
+
+export const PokemonStatBar = ({ type, value }: PokemonStatBarProps) => {
+  const { theme } = useTheme();
+
+  let iconType: IconName = 'Heart';
+  let maxRange = 150;
+
+  if (type === 'attack') {
+    iconType = 'Arm';
+    maxRange = 165;
+  }
+
+  if (type === 'defense') {
+    iconType = 'Shield';
+    maxRange = 168;
+  }
+
+  return (
+    <div className={cx('pokemonStatBar', { 'pokemonStatBar__progress--color-darkmode': theme === 'darkmode' })}>
+      <span className={cx('pokemonStatBar__values')}>
+        <Icon icon={iconType} />
+        <p className={cx('paragraph-s-medium')}>{value}</p>
+      </span>
+
+      <progress max={maxRange} value={value}></progress>
+    </div>
+  );
+};
